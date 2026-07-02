a820a241a4ce44d69cce0e97bdf3a7659376a0df
ulubahsigokhan <219955526+ulubahsigokhan@users.noreply.github.com>
2026-07-02 00:00:38 -0700

    Add browser configuration

--- src/main/java/com/gokhan/config/FrameworkConfig.java
+++ src/main/java/com/gokhan/config/FrameworkConfig.java
@@ -4,5 +4,6 @@ public class FrameworkConfig {
 
     public static final String BASE_URL = "https://practice.expandtesting.com";
     public static final String LOGIN_PATH = "/login";
+    public static final String BROWSER = "firefox";
     public static final int DEFAULT_WAIT_SECONDS = 10;
 }
--- src/test/java/com/gokhan/base/BaseTest.java
+++ src/test/java/com/gokhan/base/BaseTest.java
@@ -1,8 +1,12 @@
 package com.gokhan.base;
 
+import com.gokhan.config.FrameworkConfig;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -12,10 +16,22 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.setPageLoadStrategy(org.openqa.selenium.PageLoadStrategy.EAGER);
 
-        driver = new ChromeDriver(options);
+        if (FrameworkConfig.BROWSER.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+            driver = new ChromeDriver(options);
+        }
+        else if (FrameworkConfig.BROWSER.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        }
+        else if (FrameworkConfig.BROWSER.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        }
+        else {
+            throw new RuntimeException("Unsupported browser: " + FrameworkConfig.BROWSER);
+        }
+
         driver.manage().window().maximize();
     }
 
