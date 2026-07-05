f1affa027069a1a52f67609fbf4de4ee8dc97d5d
ulubahsigokhan <219955526+ulubahsigokhan@users.noreply.github.com>
2026-07-04 23:50:34 -0700

    Clean LoginPage imports

--- src/main/java/com/gokhan/pages/LoginPage.java
+++ src/main/java/com/gokhan/pages/LoginPage.java
@@ -3,8 +3,6 @@ package com.gokhan.pages;
 import com.gokhan.base.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class LoginPage extends BasePage {
 
