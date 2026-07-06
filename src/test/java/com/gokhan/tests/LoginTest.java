65d894542405430f7ef8747c48a192fad0144b2e
ulubahsigokhan <219955526+ulubahsigokhan@users.noreply.github.com>
2026-07-06 00:48:42 -0700

    Add flash message assertion helper

--- src/test/java/com/gokhan/tests/LoginTest.java
+++ src/test/java/com/gokhan/tests/LoginTest.java
@@ -4,19 +4,22 @@ import com.gokhan.base.BaseTest;
 import com.gokhan.config.FrameworkConfig;
 import com.gokhan.pages.LoginPage;
 import com.gokhan.testdata.LoginTestData;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
-    private static final Logger log = LoggerFactory.getLogger(LoginTest.class);
-
     private LoginPage openLoginPage() {
         driver.get(FrameworkConfig.BASE_URL + FrameworkConfig.LOGIN_PATH);
         return new LoginPage(driver);
     }
+
+    private void assertFlashMessageContains(LoginPage loginPage, String expectedMessage) {
+        Assert.assertTrue(loginPage.isFlashMessageDisplayed(), "Flash message was not displayed");
+        String actualResult = loginPage.getFlashMessage();
+        Assert.assertTrue(actualResult.contains(expectedMessage), "Flash message text was not correct");
+    }
+
 
     @Test
     public void validLoginTest() {
@@ -24,11 +27,7 @@ public class LoginTest extends BaseTest {
         LoginPage loginPage = openLoginPage();
         loginPage.login(LoginTestData.VALID_USERNAME, LoginTestData.VALID_PASSWORD);
 
-        Assert.assertTrue(loginPage.isFlashMessageDisplayed(), "Flash message was not displayed");
-
-        String actualResult = loginPage.getFlashMessage();
-        String expectedResult = LoginTestData.SUCCESS_MESSAGE;
-        Assert.assertTrue(actualResult.contains(expectedResult), "Login failed!");
+        assertFlashMessageContains(loginPage, LoginTestData.SUCCESS_MESSAGE);
     }
 
     @Test
@@ -37,10 +36,6 @@ public class LoginTest extends BaseTest {
         LoginPage loginPage = openLoginPage();
         loginPage.login(LoginTestData.INVALID_USERNAME, LoginTestData.INVALID_PASSWORD);
 
-        Assert.assertTrue(loginPage.isFlashMessageDisplayed(), "Flash message was not displayed");
-
-        String actualResult = loginPage.getFlashMessage();
-        String expectedResult = LoginTestData.INVALID_PASSWORD_MESSAGE;
-        Assert.assertTrue(actualResult.contains(expectedResult), "Invalid login error message was not displayed");
+        assertFlashMessageContains(loginPage, LoginTestData.INVALID_PASSWORD_MESSAGE);
     }
 }
