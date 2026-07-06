571ace01a59b192066d45160f9765bf5984f4fda
ulubahsigokhan <219955526+ulubahsigokhan@users.noreply.github.com>
2026-07-06 01:36:58 -0700

    Refactor login tests with DataProvider

--- src/test/java/com/gokhan/tests/LoginTest.java
+++ src/test/java/com/gokhan/tests/LoginTest.java
@@ -5,9 +5,18 @@ import com.gokhan.config.FrameworkConfig;
 import com.gokhan.pages.LoginPage;
 import com.gokhan.testdata.LoginTestData;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
+
+    @DataProvider
+    private Object[][] loginData() {
+        return new Object[][] {
+                {LoginTestData.VALID_USERNAME, LoginTestData.VALID_PASSWORD, LoginTestData.SUCCESS_MESSAGE},
+                {LoginTestData.INVALID_USERNAME, LoginTestData.INVALID_PASSWORD, LoginTestData.INVALID_PASSWORD_MESSAGE}
+        };
+    }
 
     private LoginPage openLoginPage() {
         driver.get(FrameworkConfig.BASE_URL + FrameworkConfig.LOGIN_PATH);
@@ -22,21 +31,10 @@ public class LoginTest extends BaseTest {
                 "Flash message text was not correct. Expected: " + expectedMessage + " Actual: " + actualResult);
     }
 
-    @Test
-    public void validLoginTest() {
-
+    @Test(dataProvider = "loginData")
+    public void loginShouldDisplayExpectedFlashMessage(String username, String password, String expectedMessage) {
         LoginPage loginPage = openLoginPage();
-        loginPage.login(LoginTestData.VALID_USERNAME, LoginTestData.VALID_PASSWORD);
-
-        assertFlashMessageContains(loginPage, LoginTestData.SUCCESS_MESSAGE);
-    }
-
-    @Test
-    public void invalidLoginTest() {
-
-        LoginPage loginPage = openLoginPage();
-        loginPage.login(LoginTestData.INVALID_USERNAME, LoginTestData.INVALID_PASSWORD);
-
-        assertFlashMessageContains(loginPage, LoginTestData.INVALID_PASSWORD_MESSAGE);
+        loginPage.login(username, password);
+        assertFlashMessageContains(loginPage, expectedMessage);
     }
 }
